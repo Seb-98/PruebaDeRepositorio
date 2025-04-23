d5d5d7c58d2419a226957ab764918306e756e39c
Sebastian Buelga <sebastian98@outlook.com.ar>
2025-04-23 20:28:41 -0300

    primera actividad practica

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let nombre = 'Sebastian';
+let edad = 26;
+let alumno = true;
+
+console.log("hola mi nombre es " + nombre);
+console.log("mi edad es " + edad);
+console.log("soy alumno de CoderHouse? " + alumno);
